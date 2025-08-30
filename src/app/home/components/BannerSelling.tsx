a8f886f2ec17c8b744880befaee278ecc0021b76
Md Junaead <junaead12364@gmail.com>
2025-08-30 21:05:07 +0600

    banner calculator design done

--- src/app/home/components/BannerSelling.tsx
+++ src/app/home/components/BannerSelling.tsx
@@ -15,7 +15,11 @@ const BannerSelling = () => {
             <div className=' bg-[#00000033] border border-[#505152] rounded-[40px] pt-8  pb-11 mt-[73px]  '>
                 <div className='flex justify-between items-start lg:items-center mb-4  ps-4 lg:ps-8 pe-4'>
                     <h3 className=' text-body lg:text-h3 text-white leading-normal font-medium lg:font-semibold '>Sell from day one - even  <br className='lg:hidden block' /> with zero audience</h3>
-                    <button className='text-neutral-50  text-body-sm lg:text-body leading-[17px] py-2 lg:py-3 px-4 lg:px-5 border hover:bg-white transition-all ease-in-out duration-300 border-neutral-50 cursor-pointer rounded-[100px]  hover:text-black' onClick={handleCalculatorToggle}>Open calculator</button>
+                    <button className='text-neutral-50  text-body-sm lg:text-body leading-[17px] py-2 lg:py-3 px-4 lg:px-5 border hover:bg-white transition-all ease-in-out duration-300 border-neutral-50 cursor-pointer rounded-[100px]  hover:text-black' onClick={handleCalculatorToggle}>
+                        {
+                            openCalculator ? "Close calculator" : "Open calculator"
+                        }
+                    </button>
                 </div>
                 <CosellHeroCalculator openCalculator={openCalculator} />
                 <BannerMarquee />
